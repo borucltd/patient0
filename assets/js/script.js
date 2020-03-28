f0f63d59a5b35e4871e87a4b925b686f4b110117
craigfbarry <craigfbarry@gmail.com>
2020-03-28 13:25:13 +1030

    Fixed an issue with selecting date

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var api_key = "2Xe0Yym5p1wnVlIvGnsLtH8aU4gfdgp12b1d75nR";
 var date = randomDate();
 var queryURL = "https://api.nasa.gov/planetary/apod?date=" + date + "&api_key=" + api_key;
 
+//Function to return a random date in the correct format starting from the year 2000.
 function randomDate(){
     var dateString;
     var year = 2000 + (Math.floor(Math.random()*20));
@@ -11,10 +12,10 @@ function randomDate(){
         var day = (Math.floor(Math.random()*31));
     }
     else if( month == 4 || month == 6 || month == 9 || month == 11 ) {
-        var day = (Math.floor(Math.random()*30));
+        var day = ((Math.floor(Math.random()*30))+1);
     }
     else {
-        var day = (Math.floor(Math.random()*28)); 
+        var day = ((Math.floor(Math.random()*28))+1); 
     }   
     dateString = year.toString().concat("-").concat(month.toString().padStart(2,"0")).concat("-").concat(day.toString().padStart(2,"0"));
     return dateString
